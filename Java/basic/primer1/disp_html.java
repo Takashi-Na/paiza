2c1b7ba6ba848021d5a1931666f918822e2fdfe7
Takashi-Na <69133170+Takashi-Na@users.noreply.github.com>
2021-06-28 00:00:35 +0900

    learning

---
+++ Java/basic/primer1/disp_html.java
@@ -0,0 +1,7 @@
+package Java.basic.primer1;
+
+public class disp_html {
+  public static void main(String[] args) {
+    System.out.println("<p>勇者は、荒野を歩いていた</p>");
+  }
+}
---
+++ Java/basic/primer1/hello.java
@@ -0,0 +1,7 @@
+package Java.basic.primer1;
+
+public class hello {
+  public static void main(String[] args) {
+    System.out.println("hello paizaラーニング");
+  }
+}
