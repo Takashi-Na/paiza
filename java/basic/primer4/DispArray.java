398ca972b3c457e787a2debd44e4ec624579c196
Takashi-Na <69133170+Takashi-Na@users.noreply.github.com>
2021-07-05 10:39:21 +0900

    java learning

--- java/basic/primer4/DispArray.java
+++ java/basic/primer4/DispArray.java
@@ -1,5 +1,11 @@
 package java.basic.primer4;
 
 public class DispArray {
-  
+  public static void main(String[] args) {
+    String[] array = { "戦士", "侍", "僧侶", "魔法使い" };
+
+    for (String job : array) {
+      System.out.println(job);
+    }
+  }
 }
